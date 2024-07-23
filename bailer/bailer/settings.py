52350acb13d3a3f036076c1c72b01b3f9754e771
atishayjain2003 <atishayjain2003@gmail.com>
2024-07-24 00:10:07 +0530

    migrating to postgre

--- bailer/bailer/settings.py
+++ bailer/bailer/settings.py
@@ -79,13 +79,17 @@ WSGI_APPLICATION = 'bailer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-'''DATABASES = {
+DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test',
+        'USER': 'postgres',
+        'PASSWORD': 'Atishay123456#',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-'''
+
 
 
 # Password validation
